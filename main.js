d7e297435bf6e2aa86f35f464e4030fa18f3b684
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-03 14:43:49 -0400

    finished the exercise, wrote a function to get the test passing

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-function removeOddSum() {
+function removeOddSum(array, key1, key2) {
+  newArray = array.filter(element => ((element[key1] + element[key2]) % 2 == 0));
+  return newArray;
 }
 
 module.exports = removeOddSum;
